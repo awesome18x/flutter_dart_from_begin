cc747632b8be944af1db72fd76538f570e212339
duynv1895 <duynv@ql6625.fr>
2021-01-06 09:57:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dart_tutorial/transaction.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+        id: '1',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now()
+    )
+  ];
+
+  String titleInput;
+  String amountInput;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter app'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      // border: OutlineInputBorder(),
+                      labelText: 'Title',
+                    ),
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      // border: OutlineInputBorder(),
+                      labelText: 'Amout',
+                    ),
+                    onChanged: (val) {
+                      amountInput = val;
+                    },
+                  ),
+                  FlatButton(
+                      onPressed: () {
+                        print(titleInput);
+                        print(amountInput);
+                      },
+                      child: Text('Add Transaction')
+                  )
+
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)
+                      ),
+                      child: Text(
+                        '\$${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        )
+                      )
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title, style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        )),
+                        Text(
+                            DateFormat('dd-MM-yyyy').format(tx.date),
+                          style: TextStyle(
+                            color: Colors.grey
+                          )
+                        )
+                      ]
+
+                    )
+                  ],
+                )
+              );
+            }).toList(),
+          ),
+        ]
+      )
+    );
+  }
+}
+
+
